6910bdb7ecf07e96d2d692a1807a68bf48fce37f
tori-goodell <tori.goodell@kellogg.northwestern.edu>
2022-04-14 02:32:06 +0000

    crm and school exercises - need to review

--- crm/crm.sql
+++ crm/crm.sql
@@ -1,5 +1,29 @@
 -- Drops existing tables, so we start fresh with each
+DROP TABLE IF EXISTS salespeople;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS communications;
+DROP TABLE IF EXISTS industries;
+DROP TABLE IF EXISTS industries_cat;
+
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+
+CREATE TABLE contacts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    phone_number TEXT,
+    address TEXT,
+    company_id INTEGER
+);
+CREATE TABLE companies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    email TEXT,
+    phone_number TEXT,
+    company_id INTEGER
+);
